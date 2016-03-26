b2ff08fdea3f5f4176ee3fbf2f7502e1df613113
SLucky <shahidakawsar@gmail.com>
2016-03-26 11:51:19 -0400

    initial commit

---
+++ workspace/homework1/src/numberAssignment/NumberOutput.java
@@ -0,0 +1,17 @@
+package numberAssignment;
+
+public class NumberOutput {
+
+	public static void main(String[] args) {
+		int num1 = 500;
+		int num2 = 200;
+		
+		int substraction = num1-num2;
+		System.out.println("The value is:");
+		System.out.println(substraction);
+
+		
+
+	}
+
+}
---
+++ workspace/homework1/src/stringAssignment/StringOutput.java
@@ -0,0 +1,10 @@
+package stringAssignment;
+
+public class StringOutput {
+
+	public static void main(String[] args) {
+		System.out.println("This is the first Assignment");
+
+	}
+
+}
